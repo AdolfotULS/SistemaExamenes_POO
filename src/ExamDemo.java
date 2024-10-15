d95a407c16fa887aff39dff499637c030144befb
Adolfo <166766031+AdolfotULS@users.noreply.github.com>
2024-10-15 11:22:18 -0300

    Final

--- src/ExamDemo.java
+++ src/ExamDemo.java
@@ -1,11 +1,12 @@
 /**
  * @author IGNACIA MIRANDA
  * @author ADOLFO TOLEDO
-
  */
-public class ExamDemo {
+
+ public class ExamDemo {
     public static void main(String[] args) {
-        
+        // Create an instance of the console interface and show the main menu
+        InterfazConsola interfaz = new InterfazConsola();
+        interfaz.mostrarMenu();
     }
-    
 }
